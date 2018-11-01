01d52bdeeee8ea6a69b4f2ff14d5c50fcb771c27
Changnam Hong <hcn1519@gmail.com>
2018-11-02 01:29:05 +0900

    CH 31 Finish

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/searchbar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyAvYuaUZuJOA3W9FUbKEXz53MaCE-aBzeM';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// 새로운 Component 생성, 이를 html로 전환
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    YTSearch({key: API_KEY, term: '서핑'}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={(selectedVideo) => this.setState({selectedVideo})}
+          videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+// 받은 html을 화면에 전달(DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
